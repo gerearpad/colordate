f366909a659fcf44d9623bf9513fe58784c3185d
Beniamin Kis <bkis@smallfootprint.com>
2016-04-08 12:06:45 +0300

    shortened questions and took out some.

--- app-services/questions.service.local-storage.js
+++ app-services/questions.service.local-storage.js
@@ -24,28 +24,10 @@
                         {
                             text: 'What do you do to stop your roommates eating your food?',
                             answers: [
-                                {text: 'Write your name on all your food, right down to individual yogurts.', points: 1, id: 1},
+                                {text: 'Write your name on all your food.', points: 1, id: 1},
                                 {text: 'Bring it up in conversation.', points: 2, id: 2},
                                 {text: 'Lick it all. Lick everything. But don’t tell them.', points: 3, id: 3},
                                 {text: 'Start eating their food.', points: 4, id: 4}
-                            ]
-                        },
-                        {
-                            text: 'Someone is using your Wi-Fi instead of paying for their own. What do you do?',
-                            answers: [
-                                {text: 'Let it go. Life’s too short.', points: 1, id: 5},
-                                {text: 'Change the network name to “Not Your Wi-Fi” to drop them a hint.', points: 2, id: 6},
-                                {text: 'Keep changing the password so they can’t keep logging in.', points: 3, id: 7},
-                                {text: 'Get rid of it all together, and go live in the woods.', points: 4, id: 8}
-                            ]
-                        },
-                        {
-                            text: 'Someone in front of you at the cinema is rocking their chair back and forth, annoying you. How do you deal with it?',
-                            answers: [
-                                {text: 'Move seats.', points: 1, id: 9},
-                                {text: 'Ask them to stop.', points: 2, id: 10},
-                                {text: 'Keep kicking the back of their seat until they get the message.', points: 3, id: 11},
-                                {text: 'Kick them in the head.', points: 4, id: 12}
                             ]
                         },
                         {
@@ -72,45 +54,27 @@
                     text: 'Personality',
                     questions: [
                         {
-                            text: 'You arrive at a house party but no one you know is there yet. What do you do?',
+                            text: 'You arrive at a party but no one you know is there yet. What do you do?',
                             answers: [
                                 {text: 'Get the hell out of there and try again later', points: 1, id: 21},
-                                {text: 'Make yourself invisible by standing in the corner and staring at your phone', points: 2, id: 22},
-                                {text: 'See if you can find the host and introduce yourself – it\'s only polite', points: 4, id: 23},
-                                {text: 'Grab a drink and go and join a conversation – new friends aren\'t going to make themselves!', points: 6, id: 24}
+                                {text: 'Make yourself invisible by staring at your phone', points: 2, id: 22},
+                                {text: 'See if you can find the host and introduce yourself', points: 4, id: 23},
+                                {text: 'Grab a drink and go and join a conversation', points: 6, id: 24}
                             ]
                         },
                         {
-                            text: 'Your phone rings but you don\'t recognise the number, what do you do?',
+                            text: 'Your phone rings but you don\'t know who it is, what do you do?',
                             answers: [
                                 {text: 'Throw your phone into a river and emigrate to Peru', points: 1, id: 25},
-                                {text: 'Quickly google the number to try and find out who it could be', points: 2, id: 26},
+                                {text: 'Google the number to try and find out who it could be', points: 2, id: 26},
                                 {text: 'Pick up, but only if you aren\'t doing anything', points: 3, id: 27},
                                 {text: 'Pick it up, obviously – it might be important', points: 4, id: 28}
                             ]
                         },
                         {
-                            text: 'Your colleagues are having a light-hearted conversation round the table at the pub. You are...',
-                            answers: [
-                                {text: 'At home watching Netflix on the sofa', points: 1, id: 29},
-                                {text: 'Talking when you feel comfortable, but mostly listening from the fringes', points: 2, id: 30},
-                                {text: 'Chipping in with jokes quite often and taking control of the conversation when you can', points: 3, id: 31},
-                                {text: 'Leading the conversation, and probably making people laugh in the process', points: 4, id: 32}
-                            ]
-                        },
-                        {
-                            text: 'How do you like to spend your week nights?',
-                            answers: [
-                                {text: 'At home relaxing, watching TV, on the internet or with a good book', points: 1, id: 33},
-                                {text: 'I like to go out and see people once or twice a week when I can', points: 2, id: 34},
-                                {text: 'I like to make as many plans as possible, being alone is boring', points: 3, id: 35},
-                                {text: 'I go along with most plans because of my crippling fear of missing out', points: 4, id: 35}
-                            ]
-                        },
-                        {
                             text: 'How do you feel when making small talk?',
                             answers: [
-                                {text: 'Always extremely worried of what the other person thinks of me', points: 1, id: 36},
+                                {text: 'Always worried of what the other person thinks of me', points: 1, id: 36},
                                 {text: 'A bit awkward, but I cope fine – it\'s a part of life', points: 2, id: 37},
                                 {text: 'It\'s OK, but I prefer to listen than to talk about myself', points: 3, id: 38},
                                 {text: 'Great – it\'s always fun to get to know new people', points: 4, id: 39}
@@ -140,24 +104,6 @@
                             ]
                         },
                         {
-                            text: 'Everyone should decide their college major based on...',
-                            answers: [
-                                {text: 'Whatever ensures the most financial security', points: 1, id: 48},
-                                {text: 'Their family\'s advice', points: 2, id: 49},
-                                {text: 'Whatever leads to a job they will love', points: 3, id: 50},
-                                {text: 'Whatever feels right', points: 4, id: 51}
-                            ]
-                        },
-                        {
-                            text: 'What is the foundation of a solid romantic relationship?',
-                            answers: [
-                                {text: 'Shared interests', points: 1, id: 52},
-                                {text: 'Communication', points: 2, id: 53},
-                                {text: 'Trust', points: 3, id: 54},
-                                {text: 'Chemistry', points: 4, id: 55}
-                            ]
-                        },
-                        {
                             text: 'What version of yourself would you hate to see 10 years from now?',
                             answers: [
                                 {text: 'Someone financially struggling', points: 1, id: 56},
@@ -169,7 +115,6 @@
                     ]
                 }
             ];
-
         }
 
         function GetByCategory(categoryName) {
